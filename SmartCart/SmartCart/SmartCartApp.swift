20baa31273f98324a2e1bf45a2c0536652642090
heemalsyangbo <heemalsyanbgo123@gmail.com>
2026-02-08 13:30:38 -0500

    Initial SwiftUI iOS project setup

---
+++ SmartCart/SmartCart/SmartCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCartApp.swift
+//  SmartCart
+//
+//  Created by Heemal Syangbo on 2026-02-08.
+//
+
+import SwiftUI
+
+@main
+struct SmartCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
